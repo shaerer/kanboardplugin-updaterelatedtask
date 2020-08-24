f5e6a507a763b0472e5f12b7e38cb5d9e31785a5
Stefan Härer <stefan.haerer@mvv-muenchen.de>
2020-08-24 16:36:17 +0200

    change Template folder (part 2)
    now change text and function names in files in Template folder

--- Template/update_related_task/update_related_task_button.php
+++ Template/update_related_task/update_related_task_button.php
@@ -1,5 +1,5 @@
 <?php if ($task['is_active'] == 1): ?>
 <li>
-<?= $this->modal->confirm('clock-o', t('Add time interval'), 'TaskIntervalController', 'confirm', array('plugin' => 'TaskIntervalButton', 'task_id' => $task['id'], 'project_id' => $task['project_id'])) ?>
+<?= $this->modal->confirm('clock-o', t('Update related Task'), 'UpdateRelatedTaskController', 'confirm', array('plugin' => 'UpdateRelatedTaskButton', 'task_id' => $task['id'], 'project_id' => $task['project_id'])) ?>
 </li>
 <?php endif ?>
--- Template/update_related_task/update_related_task_confirm.php
+++ Template/update_related_task/update_related_task_confirm.php
@@ -1,15 +1,15 @@
 <div class="page-header">
-    <h2><?= t('Add interval') ?></h2>
+    <h2><?= t('Update related task') ?></h2>
 </div>
 
 <div class="confirm">
     <p class="alert alert-info">
-        <?= t('Do you really want to add interval (30 minutes) to this task: "%s"?', $this->text->e($task['title'])) ?>
+        <?= t('Do you really want to update the related task: "%s"?', $this->text->e($task['title'])) ?>
     </p>
 
     <?= $this->modal->confirmButtons(
-        'TaskIntervalController',
-        'addInterval',
-        array('plugin' => 'TaskIntervalButton', 'task_id' => $task['id'], 'project_id' => $task['project_id'])
+        'UpdateRelatedTaskController',
+        'updateRelatedTask',
+        array('plugin' => 'UpdateRelatedTaskButton', 'task_id' => $task['id'], 'project_id' => $task['project_id'])
     ) ?>
 </div>
